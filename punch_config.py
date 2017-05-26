15da04faeb684ea18d078907e041187bf02cedfd
Vladimir Bolshakov <vovanbo@gmail.com>
2017-05-26 13:59:36 +0300

    Version updated from 0.1.0 to 0.1.1

---
+++ punch_config.py
@@ -0,0 +1,24 @@
+__config_version__ = 1
+
+GLOBALS = {
+    'serializer': '{{major}}.{{minor}}.{{patch}}',
+}
+
+FILES = [
+    {
+        'path': 'setup.py',
+        'serializer': "version='{{ GLOBALS.serializer }}',"
+    },
+    {
+        'path': 'aiohttp_json_api/__init__.py',
+        'serializer': "__version__ = '{{ GLOBALS.serializer }}'"
+    },
+]
+
+VERSION = ['major', 'minor', 'patch']
+
+VCS = {
+    'name': 'git',
+    'commit_message':
+        "Version updated from {{ current_version }} to {{ new_version }}",
+}
