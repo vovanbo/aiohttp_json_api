957f3e82f13dc8a9bd09d40a25c1f65847e144b8
Vladimir Bolshakov <vovanbo@gmail.com>
2017-06-13 12:21:11 +0300

    Fix bug with arguments handling in JSON API content decorator

--- aiohttp_json_api/decorators.py
+++ aiohttp_json_api/decorators.py
@@ -1,19 +1,21 @@
 from functools import wraps
 
+from aiohttp import web
 from boltons.iterutils import first
 
 from .context import RequestContext
 from .errors import HTTPUnsupportedMediaType
-from .const import JSONAPI_CONTENT_TYPE
+from .const import JSONAPI, JSONAPI_CONTENT_TYPE
 
 
 def jsonapi_content(handler):
     @wraps(handler)
     async def wrapper(*args, **kwargs):
-        context = kwargs.get('context')
-        if context is None:
-            context = first(args, key=lambda v: isinstance(v, RequestContext))
-        assert context
+        request = kwargs.get('request')
+        if request is None:
+            request = first(args, key=lambda v: isinstance(v, web.Request))
+        context = request[JSONAPI]
+        assert context and isinstance(context, RequestContext)
 
         if context.request.content_type != JSONAPI_CONTENT_TYPE:
             raise HTTPUnsupportedMediaType(
