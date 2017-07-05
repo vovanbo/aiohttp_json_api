b62d1a1d8dced439f4f920d8c4af09d1f319ce5d
Vladimir Bolshakov <bolshakov@charge.auto>
2017-07-05 16:47:49 +0300

    Version updated from 0.8.0 to 0.8.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='aiohttp_json_api',
-    version='0.8.0',
+    version='0.8.1',
     description="JSON API driven by aiohttp",
     long_description=readme + '\n\n' + history,
     author="Vladimir Bolshakov",
