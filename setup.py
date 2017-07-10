6c3cabd0ebdf5a720bbed7eab01d8371d5d5f01c
Vladimir Bolshakov <vovanbo@gmail.com>
2017-07-10 16:20:27 +0300

    Version updated from 0.9.3 to 0.10.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='aiohttp_json_api',
-    version='0.9.3',
+    version='0.10.0',
     description="JSON API driven by aiohttp",
     long_description=readme + '\n\n' + history,
     author="Vladimir Bolshakov",
