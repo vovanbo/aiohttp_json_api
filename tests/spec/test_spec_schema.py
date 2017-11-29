ffddc7560df83166c3377a376c60419725a74db8
Vladimir Bolshakov <vovanbo@gmail.com>
2017-11-29 18:21:57 +0300

    Add test for fetch single resource, 404 and 400

--- tests/spec/test_spec_schema.py
+++ tests/spec/test_spec_schema.py
@@ -1,5 +1,6 @@
 import pytest
 from aiohttp import hdrs
+from jsonpointer import resolve_pointer
 
 from aiohttp_json_api.common import JSONAPI_CONTENT_TYPE
 
@@ -36,3 +37,39 @@ async def test_content_negotiation(test_client, fantasy_app):
         headers={hdrs.ACCEPT: 'application/vnd.api+json; foo=bar'}
     )
     assert response.status == 406
+
+
+async def test_fetch_single_resource(test_client, fantasy_app):
+    client = await test_client(fantasy_app)
+    response = await client.get(
+        '/api/books/1',
+        headers={hdrs.ACCEPT: 'application/vnd.api+json'}
+    )
+    data = await response.json(content_type=JSONAPI_CONTENT_TYPE)
+
+    assert response.status == 200
+    assert resolve_pointer(data, '/data/type') == 'books'
+    assert resolve_pointer(data, '/data/id') == '1'
+    assert resolve_pointer(data, '/data/attributes/title') == \
+           'The Fellowship of the Ring'
+    assert resolve_pointer(data, '/data/relationships/author')
+    assert resolve_pointer(data, '/data/relationships/series')
+    assert resolve_pointer(data, '/data/links/self')
+
+
+async def test_fetch_resource_not_found(test_client, fantasy_app):
+    client = await test_client(fantasy_app)
+    response = await client.get(
+        '/api/books/999999',
+        headers={hdrs.ACCEPT: 'application/vnd.api+json'}
+    )
+    assert response.status == 404
+
+
+async def test_fetch_bad_request(test_client, fantasy_app):
+    client = await test_client(fantasy_app)
+    response = await client.get(
+        '/api/books/foo',
+        headers={hdrs.ACCEPT: 'application/vnd.api+json'}
+    )
+    assert response.status == 400
