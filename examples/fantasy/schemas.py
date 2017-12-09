7e5ab0765733b36dbc8d130cd2ef3207bb2bf4ec
Vladimir Bolshakov <vovanbo@gmail.com>
2017-12-09 08:00:46 +0300

    Update fantasy example

--- examples/fantasy/schemas.py
+++ examples/fantasy/schemas.py
@@ -1,36 +1,10 @@
-from aiohttp_json_api.errors import ResourceNotFound
 from aiohttp_json_api.schema import BaseSchema
-from aiohttp_json_api.fields import attributes, relationships, decorators
+from aiohttp_json_api.fields import attributes, relationships
 
-import examples.fantasy.tables as tbl
 from examples.fantasy.models import Author, Store, Book, Series, Photo, Chapter
 
 
-class CommonQueryMixin:
-    async def fetch_resource(self, resource_id, context, **kwargs):
-        async with self.app['db'].acquire() as connection:
-            result = await self.resource_class.fetch_one(connection,
-                                                         resource_id)
-
-        if result is None:
-            raise ResourceNotFound(type=self.type, id=resource_id)
-
-        return result
-
-    async def query_collection(self, context, **kwargs):
-        async with self.app['db'].acquire() as connection:
-            results = await self.resource_class.fetch_many(connection)
-
-        return results.values()
-
-    async def query_resource(self, resource_id, context, **kwargs):
-        return await self.fetch_resource(resource_id, context, **kwargs)
-
-
-class AuthorSchema(CommonQueryMixin, BaseSchema):
-    resource_cls = Author
-    type = 'authors'
-
+class AuthorSchema(BaseSchema):
     name = attributes.String()
     date_of_birth = attributes.Date()
     date_of_death = attributes.Date(allow_none=True)
@@ -40,14 +14,12 @@ class AuthorSchema(CommonQueryMixin, BaseSchema):
     books = relationships.ToMany(foreign_types=('books',))
     photos = relationships.ToMany(foreign_types=('photos',), allow_none=True)
 
-    async def delete_resource(self, resource_id, context, **kwargs):
-        pass
+    class Options:
+        resource_cls = Author
+        resource_type = 'authors'
 
 
-class BookSchema(CommonQueryMixin, BaseSchema):
-    resource_cls = Book
-    type = 'books'
-
+class BookSchema(BaseSchema):
     title = attributes.String()
     date_published = attributes.Date()
     created_at = attributes.DateTime()
@@ -58,28 +30,12 @@ class BookSchema(CommonQueryMixin, BaseSchema):
     chapters = relationships.ToMany(foreign_types=('chapters',))
     photos = relationships.ToMany(foreign_types=('photos',), allow_none=True)
 
-    async def delete_resource(self, resource_id, context, **kwargs):
-        pass
-
-    @decorators.includes('author')
-    async def include_authors(self, field, resources, context, **kwargs):
-        authors_ids = set(r.author.id for r in resources)
-
-        if not authors_ids:
-            return ()
-
-        cte = Author.cte(where=(tbl.authors.c.id.in_(authors_ids)))
-
-        async with self.app['db'].acquire() as connection:
-            results = await Author.fetch_many(connection, cte)
-
-        return results.values()
+    class Options:
+        resource_cls = Book
+        resource_type = 'books'
 
 
-class ChapterSchema(CommonQueryMixin, BaseSchema):
-    resource_cls = Chapter
-    type = 'chapters'
-
+class ChapterSchema(BaseSchema):
     title = attributes.String()
     ordering = attributes.Integer()
     created_at = attributes.DateTime()
@@ -87,14 +43,12 @@ class ChapterSchema(CommonQueryMixin, BaseSchema):
 
     book = relationships.ToOne(foreign_types=('books',))
 
-    async def delete_resource(self, resource_id, context, **kwargs):
-        pass
+    class Options:
+        resource_cls = Chapter
+        resource_type = 'chapters'
 
 
-class PhotoSchema(CommonQueryMixin, BaseSchema):
-    resource_cls = Photo
-    type = 'photos'
-
+class PhotoSchema(BaseSchema):
     title = attributes.String()
     uri = attributes.String()
     created_at = attributes.DateTime()
@@ -104,14 +58,12 @@ class PhotoSchema(CommonQueryMixin, BaseSchema):
         foreign_types=('authors', 'books', 'series')  # polymorphic
     )
 
-    async def delete_resource(self, resource_id, context, **kwargs):
-        pass
+    class Options:
+        resource_cls = Photo
+        resource_type = 'photos'
 
 
-class SeriesSchema(CommonQueryMixin, BaseSchema):
-    resource_cls = Series
-    type = 'series'
-
+class SeriesSchema(BaseSchema):
     title = attributes.String()
     created_at = attributes.DateTime()
     updated_at = attributes.DateTime(allow_none=True)
@@ -119,18 +71,17 @@ class SeriesSchema(CommonQueryMixin, BaseSchema):
     books = relationships.ToMany(foreign_types=('books',))
     photos = relationships.ToMany(foreign_types=('photos',))
 
-    async def delete_resource(self, resource_id, context, **kwargs):
-        pass
+    class Options:
+        resource_cls = Series
+        resource_type = 'series'
 
 
-class StoreSchema(CommonQueryMixin, BaseSchema):
-    resource_cls = Store
-    type = 'stores'
-
+class StoreSchema(BaseSchema):
     name = attributes.String()
     created_at = attributes.DateTime()
     updated_at = attributes.DateTime(allow_none=True)
     books = relationships.ToMany(foreign_types=('books',), allow_none=True)
 
-    async def delete_resource(self, resource_id, context, **kwargs):
-        pass
+    class Options:
+        resource_cls = Store
+        resource_type = 'stores'
